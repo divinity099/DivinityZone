7d6960b3be1f6cc76a6f4a8106472633680be992
DivinityZone <89197891+divinity099@users.noreply.github.com>
2021-08-28 13:24:23 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -224,7 +224,7 @@ EMOJI_HELP = os.environ.get("EMOJI_HELP") or "⚡"
 
 # Default .alive Group
 GROUP_LINK = os.environ.get(
-    "GROUP_LINK") or "t.me/MutualanParjamban"
+    "GROUP_LINK") or "t.me/caritemanonline5"
 
 # Default .repo Bot
 OWNER_BOT = os.environ.get(
